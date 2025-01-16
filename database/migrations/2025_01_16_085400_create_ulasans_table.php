6caa8f67388aa4be6b923d079605d0c00858e908
adrianfajar123 <adrianfajar539@gmail.com>
2025-01-16 01:03:52 -0800

    schema database clear

--- database/migrations/2025_01_16_085400_create_ulasans_table.php
+++ database/migrations/2025_01_16_085400_create_ulasans_table.php
@@ -13,15 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ulasans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->text('address');
-            $table->string('status')->default('member');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ulasans');
     }
 };
